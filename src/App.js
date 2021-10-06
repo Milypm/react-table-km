f9d430736bb3a7177ea9f447a9262742012289a9
Mily <mp_morones@hotmail.com>
2021-10-05 22:03:35 -0500

    Styling App and Table components

--- src/App.js
+++ src/App.js
@@ -1,35 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import Table from './Table';
-import './App.css';
+import './main.css';
 
 const App = () => {
   const [data, setData] = useState([]);
-  // const getData = async () => {
-  //   try {
-  //     const response = await fetch('http://localhost:3001/data', { mode: 'cors' });
-  //     console.log('1');
-  //     const d = await response.json();
-  //     return d;
-  //   } catch (error) {
-  //     return error.message;
-  //   }
-  // };
+
   useEffect(() => {
     fetch('http://localhost:3001/data')
       .then(res => res.json())
       .then(data => setData(data))
       .catch(error => setData(`Error: ${error.message}`));
-    // if (data.length === 0) {
-    //   setData(getData());
-    // }
   }, []);
-  console.log('data App', data);
   const loadingError = () => {
     let s;
     if (typeof data === 'string') {
-      s = <h1>Error...</h1>
+      s = <h2 className="message">{data}</h2>
     } else {
-      s = <h1>Loading...</h1>
+      s = <h2 className="message">Loading...</h2>
     }
     return s;
   };
--- src/Table.js
+++ src/Table.js
@@ -1,94 +1,81 @@
 import React, { useState, useMemo } from 'react';
 import PropTypes from 'prop-types';
-import './App.css';
+import './main.css';
 
 const Table = (props) => {
   const { fetched } = props;
   const [data, setData] = useState(fetched);
-  console.log('data Table', data);
   const [sortBy, setSortBy] = useState('');
   const [sortOrder, setSortOrder] = useState('');
-  // const getData = async () => {
-  //   try {
-  //     const response = await fetch('http://localhost:3001/data', { mode: 'cors' });
-  //     console.log('1');
-  //     const d = await response.json();
-  //     setData(d);
-  //     return d;
-  //   } catch (error) {
-  //     return error.message;
-  //   }
-  // };
-  // useEffect(() => {
-  //   if (data.length === 0) {
-  //     getData();
-  //   }
-  // }, []);
+
   const doSort = () => {
-    let sort;
-    if (sortBy !== '') {
-      sort = [...data].sort((a, b) => {
-        if (a[sortBy] < b[sortBy]) {
-          return sortOrder === 'asc' ? -1 : 1;
-        }
-        if (a[sortBy] > b[sortBy]) {
-          return sortOrder === 'asc' ? 1 : -1;
-        }
-        return 0;
-      });
-    }
+    const sort = [...data].sort((a, b) => {
+      if (a[sortBy] < b[sortBy]) {
+        return sortOrder === 'ascending' ? -1 : 1;
+      }
+      if (a[sortBy] > b[sortBy]) {
+        return sortOrder === 'ascending' ? 1 : -1;
+      }
+      return 0;
+    });
     return sort;
   };
   useMemo(() => {
     if (sortBy !== '') {
       setData(doSort());
     }
-  }, [sortBy]);
+  }, [sortBy, sortOrder]); // eslint-disable-line react-hooks/exhaustive-deps
   const sortingFunc = (column, order) => {
-    setSortBy(column);
     if (order === '') {
-      setSortOrder('asc');
-    } else if (order === 'asc') {
-      setSortOrder('desc');
-    } else if (order === 'desc') {
-      setSortOrder('asc');
+      setSortOrder('ascending');
+    } else if (order === 'ascending') {
+      setSortOrder('descending');
+    } else if (order === 'descending') {
+      setSortOrder('ascending');
     }
+    setSortBy(column);
   };
-  // const button = (<button type="button" onClick={sortingFunc}>Sort</button>);
+  const btn = (val) => {
+    return (
+      <button type="button" className="sort-btn" onClick={() => sortingFunc(val, sortOrder)}>
+        {sortOrder === '' ? 'Sort' : sortOrder.toUpperCase()}
+      </button>
+    )
+  };
   return (
-    <table>
+    <table className="table">
       <thead>
-        <tr>
+        <tr className="table-headers">
           <th>
             Name
-            <button type="button" onClick={() => sortingFunc('name', sortOrder)}>{sortOrder === '' ? 'Sort' : sortOrder.toUpperCase()}</button>
+            {btn('name')}
           </th>
           <th>
             Address
-            <button type="button" onClick={() => sortingFunc('address', sortOrder)}>{sortOrder === '' ? 'Sort' : sortOrder.toUpperCase()}</button>
+            {btn('address')}
           </th>
           <th>
             City
-            <button type="button" onClick={() => sortingFunc('city', sortOrder)}>{sortOrder === '' ? 'Sort' : sortOrder.toUpperCase()}</button>
+            {btn('city')}
           </th>
           <th>
             Region
-            <button type="button" onClick={() => sortingFunc('region', sortOrder)}>{sortOrder === '' ? 'Sort' : sortOrder.toUpperCase()}</button>
+            {btn('region')}
           </th>
           <th>
             Country
-            <button type="button" onClick={() => sortingFunc('country', sortOrder)}>{sortOrder === '' ? 'Sort' : sortOrder.toUpperCase()}</button>
+            {btn('country')}
           </th>
           <th>
             Birthday
-            <button type="button" onClick={() => sortingFunc('birthday', sortOrder)}>{sortOrder === '' ? 'Sort' : sortOrder.toUpperCase()}</button>
+            {btn('birthday')}
           </th>
         </tr>
       </thead>
-      <tbody>
+      <tbody className="table-body">
         {
           data.map((obj) => (
-            <tr key={obj.name}>
+            <tr key={obj.name} className="table-datarows">
               <td>{obj.name}</td>
               <td>{obj.address}</td>
               <td>{obj.city}</td>
@@ -103,6 +90,6 @@ const Table = (props) => {
   );
 }
 Table.propTypes = {
-  fetched: PropTypes.arrayOf(PropTypes.shape(PropTypes.objectOf())).isRequired,
+  fetched: PropTypes.arrayOf(PropTypes.shape()).isRequired,
 };
 export default Table;
